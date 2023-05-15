1919714d6d900dcf09303ef8b39ad13138bb1066
Hussien haji <92381507+HussienHaji@users.noreply.github.com>
2023-05-15 22:11:09 +0430

    finishing testimonials  section

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const menu = document.querySelector(".menu");
 const navList = document.querySelector(".nav-list");
 const navItems = document.querySelectorAll(".nav-item");
+const personImage = document.querySelector(".person-image");
+const personName = document.querySelector(".person-name");
+const personText = document.querySelector(".person-text");
+const testiArrowLeft = document.querySelector(".la-arrow-alt-circle-left");
+const testiArrowRight = document.querySelector(".la-arrow-alt-circle-right");
+let currtentTestimonial = 0;
+
+const persons = [
+  {
+    name: "Emily White",
+    image: "./images/person.jpg",
+    text: "I had a fantastic experience shopping on this shoe website! The user-friendly interface made it easy to find the styles I was looking for, and the variety of options exceeded my expectations.",
+  },
+  {
+    name: "Thomas Johnson",
+    image: "./images/person2.jpg",
+    text: "I recently purchased a pair of shoes from this site and I couldn't be happier! The process was seamless and the shoes fit perfectly. I highly recommend this site for anyone in need of quality footwear.",
+  },
+  {
+    name: "Anthony Taylor",
+    image: "./images/person3.jpg",
+    text: "Excellent shoes, easy ordering process, and fast delivery! Highly recommend this site for anyone looking for stylish and comfortable footwear.",
+  },
+  {
+    name: "James Davis",
+    image: "./images/person4.jpg",
+    text: "The selections were vast and reasonably priced. Shipping was fast and the shoes felt high quality. I would recommend this store for quality shoes at a great value",
+  },
+];
 
 menu.addEventListener("click", () => {
   navList.classList.toggle("active");
@@ -13,3 +42,17 @@ navItems.forEach((item) => {
     }, 300);
   });
 });
+
+const changeTestimonial = (right) => {
+  if (right) currtentTestimonial++;
+  if (currtentTestimonial > persons.length - 1) currtentTestimonial = 0;
+  if (!right) currtentTestimonial--;
+  if (currtentTestimonial < 0) currtentTestimonial = persons.length - 1;
+
+  personImage.setAttribute("src", persons[currtentTestimonial].image);
+  personName.innerText = persons[currtentTestimonial].name;
+  personText.innerText = persons[currtentTestimonial].text;
+};
+
+testiArrowRight.addEventListener("click", () => changeTestimonial(true));
+testiArrowLeft.addEventListener("click", () => changeTestimonial(false));
